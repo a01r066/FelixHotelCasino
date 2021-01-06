e6541ddc05ea3512ec166a5fdaa0871dec19683f
Thanh Minh <vmthanh24@gmail.com>
2021-01-06 14:58:12 +0700

    First commit with re-structured html & css

---
+++ src/app/cook-delecious/cook-delecious.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cook-delecious',
+  templateUrl: './cook-delecious.component.html',
+  styleUrls: ['./cook-delecious.component.css']
+})
+export class CookDeleciousComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
