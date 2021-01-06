3b140207eddd7c2ca5fff12af5f5d48ae9460481
Thanh Minh <vmthanh24@gmail.com>
2021-01-06 14:58:03 +0700

    First commit with re-structured html & css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { CookDeleciousComponent } from './cook-delecious/cook-delecious.component';
+import { ServicesComponent } from './services/services.component';
+import { BookTableComponent } from './book-table/book-table.component';
+import { GetAppComponent } from './get-app/get-app.component';
+import { FeaturedFoodComponent } from './featured-food/featured-food.component';
+import { OurBlogComponent } from './our-blog/our-blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    CookDeleciousComponent,
+    ServicesComponent,
+    BookTableComponent,
+    GetAppComponent,
+    FeaturedFoodComponent,
+    OurBlogComponent
   ],
   imports: [
     BrowserModule
