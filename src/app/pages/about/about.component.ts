276a5dbba86d23c34e16a255e34877d1f83fcfc8
Thanh Minh <vmthanh24@gmail.com>
2021-01-07 14:05:30 +0700

    Upgrade bootstrap 3x -> 4x

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -6,10 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  slidePaths = [
+    '../../assets/img/slideshow/sld00.jpg',
+    '../../assets/img/slideshow/sld01.jpg',
+    '../../assets/img/slideshow/sld02.jpg',
+    '../../assets/img/slideshow/sld03.jpg',
+    '../../assets/img/slideshow/sld04.jpg',
+    '../../assets/img/slideshow/sld05.jpg',
+    '../../assets/img/slideshow/sld06.jpg',
+    '../../assets/img/slideshow/sld07.jpg',
+    '../../assets/img/slideshow/sld08.jpg',
+    '../../assets/img/slideshow/sld09.jpg',
+    '../../assets/img/slideshow/sld10.jpg',
+    '../../assets/img/slideshow/sld11.jpg',
+    '../../assets/img/slideshow/sld12.jpg',
+    '../../assets/img/slideshow/sld13.jpg',
+    '../../assets/img/slideshow/sld14.jpg',
+    '../../assets/img/slideshow/sld15.jpg',
+  ];
+
+  index = 0;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  previous(){
+    if (this.index > 0){
+      this.index -= 1;
+    } else {
+      this.index = this.slidePaths.length - 1;
+    }
+    console.log(this.slidePaths[this.index]);
+  }
+
+  next(){
+    if (this.index < this.slidePaths.length - 1){
+      this.index += 1;
+    } else {
+      this.index = 0;
+    }
+    console.log(this.slidePaths[this.index]);
+  }
 }
